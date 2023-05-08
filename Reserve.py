bd12103ea6bf78d4a8013286462be2bbc25e8171
donovanhargett <donovanhargett@gmail.com>
2023-05-08 14:46:54 -0500

    Changes

--- Reserve.py
+++ Reserve.py
@@ -5,8 +5,7 @@ import string
 def savefile(file_name, first_name, seat_row, seat_column, 
 reservation_code):
     with open(file_name, 'a') as f:
-        
-f.write(f'{first_name},{seat_row},{seat_column},{reservation_code}\n')
+        f.write(f'{first_name},{seat_row},{seat_column},{reservation_code}\n')
 
 def Userinput(length=15):
     FirstName = input("Enter First name:  ")
@@ -19,18 +18,49 @@ def Userinput(length=15):
 string.digits, k=length -2 ))
 
     # Create a reservation code using the first letter, the last letter, 
-and the random string
-    reservation_code = 
-f"{FirstName[0].upper()}{LastName[-1].upper()}{random_str}"
-
-    print(f"Congratulations {FirstName}, your seat row is {Seatrow}, and 
-the seat number is seat number {Seat}, here is your reservation code = 
-{reservation_code}")
+#and the random string
+    reservation_code = f"{FirstName[0].upper()}{LastName[-1].upper()}{random_str}"
+    print(f"Congratulations {FirstName}, your seat row is {Seatrow}, and the seat number is seat number {Seat}, here is your reservation code = {reservation_code}")
 
 
      # Save the input and reservation code to a file
     savefile("reservations.txt", FirstName, Seatrow, Seat, 
 reservation_code)
+
+
+
+#MY CODE
+def create_seat_array(reserved_seats):
+    seat_array = [['O' for _ in range(4)] for _ in range(12)]
+
+    for seat in reserved_seats:
+        row, column = seat
+        if 0 <= row < 12 and 0 <= column < 4:
+            seat_array[row][column] = 'X'
+
+    return seat_array
+
+
+def read_reserved_seats(file_path):
+    reserved_seats = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            name, row, column, reservation_code = [x.strip() for x in line.strip().split(',')]
+            reserved_seats.append((int(row), int(column)))
+    return reserved_seats
+
+
+def get_cost_matrix():
+    cost_matrix = [[100, 75, 50, 100] for row in range(12)]
+    return cost_matrix
+
+def calculate_total_sales(reserved_seats, cost_matrix):
+    total_sales = 0
+    for row, column in reserved_seats:
+        if row < len(cost_matrix) and column < len(cost_matrix[0]):
+            total_sales += cost_matrix[row][column]
+    return total_sales
+
 
 
 Userinput()
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, url_for, flash, redirect, abort
 from authenticate import *
+from Reserve import *
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -26,5 +27,18 @@ def admin():
 
 
     return render_template("admin.html")
+
+def reservation_page():
+    reserved_seats = read_reserved_seats('reservations.txt')
+    seat_array = create_seat_array(reserved_seats)
+    return render_template('reservation.html', seat_array=seat_array)
+
+@app.route('/reservation/')
+def reservation_page():
+    reserved_seats = read_reserved_seats('reservations.txt')
+    seat_array = create_seat_array(reserved_seats)
+    cost_matrix = get_cost_matrix()
+    total_sales = calculate_total_sales(reserved_seats, cost_matrix)
+    return render_template('reservation.html', seat_array=seat_array, total_sales=total_sales)
 
 app.run()
